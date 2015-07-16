6c6495e1b6e639262c87cc8006b5faa4d4baee6f
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-07-15 22:22:29 -0400

    Update Application Controller to use services
    Just to make sure things are going alright with the services and
    queries, returning a list of categories to the front end.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,8 +3,13 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import services._
+import models._
+
 class Application extends Controller {
+	val applicationService = new ApplicationService()
 	def index = Action { implicit request =>
-		Ok("Hi how are ya")
+		val cats = applicationService.getCategories()
+		Ok(cats.mkString(","))
 	}
 }
