f136f7c5bd90b0e2e23842b802fef0c2bc8b0ee0
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-07-15 23:12:08 -0400

    Indent Categories in View, add depth to Model
    Add depth to model as a var, defaulting to 0
    Add nonbreaking spaces to make it easy to tell hierachy in view

--- app/models/Models.scala
+++ app/models/Models.scala
@@ -7,13 +7,21 @@ trait Content {
 
 case class Category(val id: Int, val name: String) extends Content {
 	var products : Option[List[Product]] = None
+	var depth : Int = _
+
+	/* Allow setting of depth */
+	def this(id: Int, name: String, depth: Int) = { 
+		this(id, name)
+		this.depth = depth
+	}
+
 	def getProducts() : List[Product] = {
 		products match {
 			case Some(list) => list
 			case None | Some(Nil) => Nil
 		}
-		
 	}
+
 }
 
 case class Product(val id: Int, val name: String, val categoryId: Int) extends Content 
