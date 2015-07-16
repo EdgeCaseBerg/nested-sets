34cef3c44e6e337475eff37428261526bd74f382
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-07-16 00:01:15 -0400

    Add productCount to model field

--- app/models/Models.scala
+++ app/models/Models.scala
@@ -8,11 +8,18 @@ trait Content {
 case class Category(val id: Int, val name: String) extends Content {
 	var products : Option[List[Product]] = None
 	var depth : Int = _
+	var productCount : Int = _
 
 	/* Allow setting of depth */
 	def this(id: Int, name: String, depth: Int) = { 
 		this(id, name)
 		this.depth = depth
+	}
+
+	/* Allow setting of product count */
+	def this(id: Int, name: String, depth: Int, productCount: Int) = {
+		this(id, name, depth)
+		this.productCount = productCount
 	}
 
 	def getProducts() : List[Product] = {
