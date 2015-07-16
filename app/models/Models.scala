9a2e93c3cc656c128bedc89e9b1807df0b97e190
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-07-15 22:21:10 -0400

    Update Category Model with possible products
    Create var Option to contain a products for a category. so we can
    populate it from the service without needing to pass it as a parameter
    for creating categories in the first place.

--- app/models/Models.scala
+++ app/models/Models.scala
@@ -5,6 +5,15 @@ trait Content {
 	def name: String
 }
 
-case class Category(val id: Int, val name: String) extends Content
+case class Category(val id: Int, val name: String) extends Content {
+	var products : Option[List[Product]] = None
+	def getProducts() : List[Product] = {
+		products match {
+			case Some(list) => list
+			case None | Some(Nil) => Nil
+		}
+		
+	}
+}
 
 case class Product(val id: Int, val name: String, val categoryId: Int) extends Content 
